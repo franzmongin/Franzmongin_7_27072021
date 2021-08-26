ce94adb7c89becb11b9faefc3f8f1fb6868bdc39
franzmongin <franz.mongin@gmail.com>
2021-08-26 14:14:11 +0200

    reformat algo1

--- js/orderRecipesWithActiveTagsAndSearchValue.js
+++ js/orderRecipesWithActiveTagsAndSearchValue.js
@@ -3,24 +3,44 @@ export function orderRecipesWithActiveTagsAndSearchValue(
   recipeArray,
   value = ""
 ) {
-  console.log(value);
-  let activeUstensils = activeSortings.ustensils.map((e) => e.toLowerCase());
-  let activeAppliances = activeSortings.appliances.map((e) => e.toLowerCase());
-  let activeIngredients = activeSortings.ingredients.map((e) =>
-    e.toLowerCase()
-  );
-  return recipeArray.filter((e) => {
-    let ingArray = [];
-    e.ingredients.forEach((ing) => {
-      ingArray.push(ing.ingredient.toLowerCase());
-    });
-    return (
-      activeUstensils.every((v) => e.ustensils.includes(v)) &&
-      activeAppliances.every((v) => e.appliance.includes(v)) &&
-      activeIngredients.every((i) => ingArray.includes(i)) &&
-      (ingArray.some((i) => i.includes(value)) ||
-        e.name.includes(value) ||
-        e.description.includes(value))
+  function orderWithActiveTags() {
+    let activeUstensils = activeSortings.ustensils.map((e) => e.toLowerCase());
+    let activeAppliances = activeSortings.appliances.map((e) =>
+      e.toLowerCase()
     );
-  });
+    let activeIngredients = activeSortings.ingredients.map((e) =>
+      e.toLowerCase()
+    );
+    return recipeArray.filter((e) => {
+      let ingArray = [];
+      e.ingredients.forEach((ing) => {
+        ingArray.push(ing.ingredient.toLowerCase());
+      });
+      return (
+        activeUstensils.every((v) => e.ustensils.includes(v)) &&
+        activeAppliances.every((v) => e.appliance.includes(v)) &&
+        activeIngredients.every((i) => ingArray.includes(i))
+      );
+    });
+  }
+  let orderedRecipesWithTags = orderWithActiveTags();
+
+  function orderWithSearchValue() {
+    return orderedRecipesWithTags.filter((e) => {
+      let ingArray = [];
+      e.ingredients.forEach((ing) => {
+        ingArray.push(ing.ingredient.toLowerCase());
+      });
+      console.log(e.name);
+      console.log(value);
+      return (
+        ingArray.some((i) => i.includes(value)) ||
+        e.name.toLowerCase().includes(value) ||
+        e.description.includes(value)
+      );
+    });
+  }
+  let newArray = orderWithSearchValue();
+  console.log(newArray);
+  return newArray;
 }
